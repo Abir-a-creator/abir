e38d6ef9d5c168efdbeef26d5657508d6b588854
Abir <abir.zar@gmail.com>
2021-02-19 21:38:28 +0100

    third commit

---
+++ src/Component/Profile/ProfilePhoto.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class ProfilePhoto extends Component {
+    render () {
+        return <img src="PhotoDeProfile.jpg" alt="PhotoDeProfile" />
+    }
+}
+export default ProfilePhoto;
+
+
